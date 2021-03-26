cc0b726375a001f38637db9f50c3fb568075556d
Decilap denis <decilapdenis@gmail.com>
2021-03-26 13:58:13 +0100

    Création menu dropdown

--- lieux/js/conf.js
+++ lieux/js/conf.js
@@ -2,7 +2,13 @@ const API_LINK = {
     api: "https://rickandmortyapi.com/api/location",
     searchByType: "https://rickandmortyapi.com/api/location?type=",
 }
-  
+
+
 module.exports = {
     API_LINK
 }
+
+
+
+
+
--- lieux/js/script.js
+++ lieux/js/script.js
@@ -1,5 +1,5 @@
 
-const {_fetchData, _replaceClass} = require('./utils');
+const {_fetchData, _replaceClass, _autocomplete, _types} = require('./utils');
 const {API_LINK} = require('./conf');
 
 class Location{
@@ -8,32 +8,36 @@ class Location{
     locations = [];
     location = {};
     constructor(){
-      _fetchData(API_LINK.api + "?page=" + this.curent_page)
-        .then((locations) => {
+        
+    _fetchData(API_LINK.api + "?page=" + this.curent_page)
+      .then((locations) => {
             let locations_bg = document.querySelector('.locations');
             const {info, results} = locations;
             this.locations = results;
             this.info = info;
-            locations_bg.innerHTML += this.init();
-            this.createLocations(this.locations);
-            this.searchByType();
+            setTimeout(() => {
+                locations_bg.innerHTML += this.init();
+                _autocomplete(document.getElementById("myInput"), _types);
+                this.createLocations(this.locations);
+                this.searchByType();
+            });
         });
-      }
+    }
 
-      init(){ 
-            return `<div class="locations_body">
-                        <h1 class="locations_title">Rick And Morty</h1>
-                        <span class="locations_count">Location (${this.info.count})</span>
-                        <a href="${this.info.next}" class="locations_link" href="">Pages suivante</a>
-                        <ul class="locations_list"></ul>
-                    </div>`;
-      }
+    init(){ 
+        
+        return `<div class="locations_body">
+                    <h1 class="locations_title">Rick And Morty</h1>
+                    <span class="locations_count">Location (${this.info.count})</span>
+                    <a href="${this.info.next}" class="locations_link" href="">Pages suivante</a>
+                    <ul class="locations_list"></ul>
+                </div>`;
+    }
 
-      createLocations(locations){
+    createLocations(locations){
         let locations_list = document.querySelector('.locations_list');
         locations_list.innerHTML = "";
         locations.forEach(location => {
-            console.log(this.location)
             let li = document.createElement('li');
             li.innerHTML += `<li>
                                 <div class="locations_card">
@@ -44,49 +48,48 @@ class Location{
                                     </div>
                                 </div>
                             </li>`;
-            li.querySelector('a')
-            .addEventListener('click', this.showDetails.bind(this));
+            li.querySelector('a').addEventListener('click', this.showDetails.bind(this));
             locations_list.append(li);
-        });
-      }
+    });
+    }
 
-      showDetails(e){
-          e.preventDefault();
-          _fetchData(e.currentTarget.dataset.url)
-          .then((location) => {
+    showDetails(e){
+        e.preventDefault();   
+        _fetchData(e.currentTarget.dataset.url)
+        .then((location) => {
             this.location = location;
             if(location.residents.length > 0){
                 Promise.all(this.location.residents.map(url => _fetchData(url)))
-                .then(resp => Promise.all( resp.map(r => r)))
+                .then(resp => Promise.all( resp.map(res => res)))
                 .then(residents => {
-                  document.querySelector('.dataList').innerHTML = this.showLocationDetails(residents);
-                  document.body.classList.add('modal-active');
-                  document.querySelector('.close').addEventListener('click', (e) => {
+                    document.querySelector('.dataList').innerHTML = this.showLocationDetails(residents);
+                    document.body.classList.add('modal-active');
+                    document.querySelector('.close').addEventListener('click', (e) => {
                         _replaceClass(e.target.parentNode, 'two', 'out');
                         document.body.classList.remove('modal-active')
-                  });
+                    });
                 });
             }
-          })  
-      }
+        })  
+    }
 
-      searchByType(){
-        let inputs = document.querySelectorAll('.search_by_type input');
-        inputs.forEach(input => {
-            input.addEventListener('click', (e)  => {
-                _fetchData(API_LINK.searchByType + e.target.value)
+    searchByType(){
+        let button_search = document.querySelector('.search');
+        button_search.addEventListener('click', (e)  => {
+            let input = e.target.previousElementSibling.querySelector('input');
+            _fetchData(API_LINK.searchByType + input.value)
                 .then((locations) => {
-                    if(e.target.value === 'All'){
-                        this.createLocations(this.locations);
-                    }else{
+                     if(input.value.trim().length > 0){
                         this.createLocations(locations.results);
+                        input.value = "";
+                     }else{
+                        this.createLocations(this.locations);
                     }
-                })
-            });
+             });
         });
-      }
+    }
 
-      showLocationDetails(residents){
+    showLocationDetails(residents){
         let str =  `<div id="modal-container" class="two">
                         <a href="#" class="close"></a>
                         <div class="modal-background">
@@ -98,32 +101,56 @@ class Location{
                         </div>
                     </div>`;
         return str;
-      }
+    }
 
-      locationDetail(resident){
-            return `<div class="locations_details">
-                        <div class="card">
-                            <div class="card-image">
-                                <img src="${resident.image}" alt="Orange" />
+    locationDetail(resident){
+        return `<div class="locations_details">
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="${resident.image}" alt="Orange"/>
+                        </div>
+                        <div class="card-body"> 
+                            <div class="card-date">
+                                <span>Type ${resident.type}</span>
+                            </div>     
+                            <div class="card-date">
+                                <time>${resident.created}</time>
                             </div>
-                            <div class="card-body"> 
-                                <div class="card-date">
-                                    <span>Type ${resident.type}</span>
-                                </div>     
-                                <div class="card-date">
-                                    <time>${resident.created}</time>
-                                </div>
-                                <div class="card-title">
+                            <div class="card-title">
                                 <h3>${resident.name}</h3>
-                                    </div>
-                                <div class="card-excerpt">
-                                    <p>${this.location.dimension}</p>
-                                </div>
+                            </div>
+                            <div class="card-excerpt">
+                                <p>${this.location.dimension}</p>
                             </div>
                         </div>
                     </div>
-            `;
-      }
+                </div>`;
+    }
+
+    getLinksType() {
+        let links = [];
+        for (let index = 0; index < 7; index++) {
+            links.push(API_LINK.api + "?page=" + index);
+        }
+        return { links };
+    }
+
+    createAllType() {
+        let listTypes = [];
+        let { links} = this.getLinksType();
+        return Promise.all(links.map(url => fetch(url)))
+                .then(resp => Promise.all(resp.map(res => res.json())))
+                .then(locations => {
+                    locations.forEach(location => {
+                        location.results.forEach(res => {
+                            listTypes.push(res.type);
+                        });
+                    });
+                    return [...new Set(listTypes)];
+                });
+    }
   }
 
   new Location();
+
+  
